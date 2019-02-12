29b0e64d3cb92a5a390f6268d68d30d997c9746a
aloneik <aloneik@yandex.ru>
2019-02-12 15:27:51 +0300

    Add files via upload

--- lesson_3/modules/rldall.py
+++ lesson_3/modules/rldall.py
@@ -1,4 +1,5 @@
 import types
+import sys
 
 
 def recursive_reload(module, visited):
@@ -11,13 +12,21 @@ def recursive_reload(module, visited):
                 recursive_reload(attrobj, visited)
 
 
-def reload_all(*args):
+def reload_all(*args, **kwargs):
     visited = {}
-    for arg in args:
-        if type(arg) == types.ModuleType:
-            recursive_reload(arg, visited)
+    if args:
+        for arg in args:
+            if type(arg) == types.ModuleType:
+                recursive_reload(arg, visited)
+    if kwargs:
+        print kwargs
+        for arg in kwargs.values():
+            if type(arg) == types.ModuleType:
+                recursive_reload(arg, visited)
 
 
 if __name__ == "__main__":
     import rldall
     reload_all(rldall)
+else:
+    reload_all(sys.modules)
